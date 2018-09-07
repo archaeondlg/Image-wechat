9640aa18eaf2a2a1a291a2e2416fd609341f715a
阿金 <sunshinedlg@163.com>
2018-09-08 02:44:34 +0800

    合并图片剪切和保存逻辑，优化代码

--- img-wechat.py
+++ img-wechat.py
@@ -6,11 +6,8 @@ from PIL import Image
 
 def fill_image(image, color='white'):
     width, height = image.size
-    # python中的三目运算
     length = width if width > height else height
-    # 新生成图片，参数：模式（一般RGB），尺寸，背景色（默认黑）
     new_image = Image.new(image.mode, (length, length), color)
-    # 在之前新纯色图片上粘贴图片
     if width > height:
         new_image.paste(image, (0, int((length - height) / 2)))
     else:
@@ -18,27 +15,19 @@ def fill_image(image, color='white'):
     return new_image
 
 
-def cut_image(image):
+def deal_image(image):
     width, height = image.size
     img_length = int(width / 3)
-    boxs = []
-    for y in xrange(3):
-        for x in xrange(3):
+    for y in range(3):
+        for x in range(3):
             box = (x * img_length, y * img_length, (x + 1) * img_length, (y + 1) * img_length)
-            boxs.append(box)
-    images = [image.crop(box) for box in boxs]
-    return images
-
-
-def save_image(images):
-    for index, item in images:
-        item.save(str(index) + '.png', 'PNG')
+            img = image.crop(box)
+            img.save(str(x) + '-' + str(y) + '.png', 'PNG')
+    return 'success'
 
 
 if __name__ == '__main__':
     file_path = 'photo.jpg'
     image = Image.open(file_path)
     image = fill_image(image, 'white')
-    image_list = cut_image(image)
-
-    save_image(image_list)
+    deal_image(image)
